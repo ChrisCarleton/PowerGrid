7605d98444309936c99da18b63f8bf7017a58f27
Chris Carleton <mrchriscarleton@gmail.com>
2016-09-29 17:06:56 -0400

    Added minification of the JS bundle.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,9 @@
+import clean from 'gulp-clean';
 import eslint from 'gulp-eslint';
 import gulp from 'gulp';
 import gls from 'gulp-live-server';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
 import util from 'gulp-util';
 import webpackStream from 'webpack-stream';
 
@@ -18,6 +21,20 @@ gulp.task('bundle', ['lint'], () => {
 	return gulp
 		.src('./web/app.jsx')
 		.pipe(webpackStream(webpackConfig))
+		.pipe(gulp.dest('public/'));
+});
+
+gulp.task('clean', () => {
+	return gulp
+		.src(['public/bundle.min.js'])
+		.pipe(clean({ read: false }));
+});
+
+gulp.task('minify', ['clean', 'bundle'], () => {
+	return gulp
+		.src('public/bundle.js')
+		.pipe(uglify())
+		.pipe(rename('bundle.min.js'))
 		.pipe(gulp.dest('public/'));
 });
 
