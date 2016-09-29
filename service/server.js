8b20bbb8b90b4152803484302b67b398806d0f62
Chris Carleton <mrchriscarleton@gmail.com>
2016-09-29 15:42:59 -0400

    Integrated Bunyan logging into Express requests.

--- service/server.js
+++ service/server.js
@@ -1,5 +1,6 @@
 import config from './config';
 import express from 'express';
+import expressLogger from 'express-bunyan-logger';
 import http from 'http';
 import log from './logger';
 import pug from 'pug';
@@ -7,6 +8,11 @@ import pug from 'pug';
 const app = express();
 const renderHomePage = pug.compileFile(__dirname + '/index.pug');
 const isProduction = (config.env === 'production');
+
+app.use(expressLogger({
+	logger: log,
+	excludes: [ 'referer', 'user-agent', 'body', 'short-body', 'response-hrtime', 'req', 'res', 'res-headers' ]
+}));
 
 app.get('/', (req, res) => {
 	res.send(renderHomePage({
