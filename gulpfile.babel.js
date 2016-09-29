6b088a53fa6d6d705516e009932ccec4767365d8
Chris Carleton <mrchriscarleton@gmail.com>
2016-09-29 15:59:44 -0400

    Added gulp-live-server as development server.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,7 @@
 import eslint from 'gulp-eslint';
 import gulp from 'gulp';
+import gls from 'gulp-live-server';
+import util from 'gulp-util';
 
 gulp.task('lint', () => {
 	return gulp
@@ -8,3 +10,15 @@ gulp.task('lint', () => {
 		.pipe(eslint.format())
 		.pipe(eslint.failOnError());
 });
+
+gulp.task('develop', () => {
+	const server = gls.new('service/index.js');
+	server.start();
+
+	gulp.watch(['service/**/*.js'], () => {
+		util.log('[gulp-live-server]', 'Change detected. Restarting server...');
+		server.start.bind(server)();
+	});
+});
+
+gulp.task('default', ['develop']);
